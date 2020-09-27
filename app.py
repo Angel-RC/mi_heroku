8686437f21db58e97d017caa76e58618722dce6c
Ángel RC <37696271+Angel-RC@users.noreply.github.com>
2020-09-27 16:25:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Original Author: https://github.com/patidarparas13,
 
 import itertools
 
-import streamlit.ReportThread as ReportThread
+import streamlit.logger 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
